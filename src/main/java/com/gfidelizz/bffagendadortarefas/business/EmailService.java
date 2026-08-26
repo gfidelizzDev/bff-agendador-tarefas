adbab33e74e1d34c6fcc55f0dd42e6304eb259c6
Gustavo <gfidelissamuel@gmail.com>
2026-08-26 14:46:20 -0300

    adicionado client de email

---
+++ src/main/java/com/gfidelizz/bffagendadortarefas/business/EmailService.java
@@ -0,0 +1,19 @@
+package com.gfidelizz.bffagendadortarefas.business;
+
+
+import com.gfidelizz.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.gfidelizz.bffagendadortarefas.infraestructure.Client.EmailClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailClient emailClient;
+
+
+    public void enviaEmail(TarefasDTOResponse dto) {
+        emailClient.enviaEmail(dto);
+    }
+}
